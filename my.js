4930efd82a92a3934e1d4a607c2bbc48011d10c9
muhammadCse36 <34022928+muhammadCse36@users.noreply.github.com>
2020-06-10 22:14:45 +0200

    few updates

--- my.js
+++ my.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     let txt = document.getElementById('name');
     txt.addEventListener('keydown', anyKey);
+    let conv = document.getElementById('convert');
+    conv.addEventListener('click',entr);
 }
 
 function anyKey(ev) {
@@ -11,8 +13,14 @@ function anyKey(ev) {
     let target = ev.currentTarget;
     let tag = target.tagName;
     let char = ev.char || ev.charCode || ev.which;
-    log(char,tag);
+    //log(char,tag);
     if (char == 190) {
         ev.preventDefault();
     }
+    let s = String.fromCharCode(char);
+    log(s);
+}
+
+function entr(en) {
+    log(char.value);
 }
